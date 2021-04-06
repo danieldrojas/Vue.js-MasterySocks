22d8f034d6c63c646ce12e333cb82fac43a6ca56
Daniel Rojas <daniel.drojas@outlook.com>
2021-04-05 22:50:15 -0500

    add v-bind for src att in img's tag

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var app = new Vue({ //ne
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'Socks are a fluffy piece of clothe that covers the feet'
+        description: 'Socks are a fluffy piece of clothe that covers the feet',
+        image: './vmSocks.jpeg',
+        link: 'https://google.com',
+        inStock: false,
     }
-})
+}) 
