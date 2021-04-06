7502bb28552f0f4a45a6e222254d0e61a65173c3
Daniel Rojas <daniel.drojas@outlook.com>
2021-04-06 12:20:20 -0500

    add computed properties

--- main.js
+++ main.js
@@ -2,38 +2,28 @@
 var app = new Vue({ //ne
     el: '#app',
     data: {
+        brand: "Vue Mastery",
         product: 'Socks',
-        description: 'Socks are a fluffy piece of clothe that covers the feet',
-        image: './vmSocks.jpeg',
         link: 'https://google.com',
+        selectedVariant: 0,
         inStock: false,
+        details: ["75% Cotton", "50% Polyesterk", "Unisex"],
         inventory:0,
-        sizes: [
+        variants: [
             {
-                size: "Small",
-                id: "s"
+                variantId: 1,
+                variantColor: "green",
+                variantImage: "./assets/vmSocks.jpeg",
+                variantQuantity: 10
 
-            }, {
-                size: "Medium",
-                id: 'm'
             },
             {
-                size: "Large",
-                id: "l"
-            }
-        ],
-        socks: [
-            {
-                id: 1,
-                color: "green",
-                image: "./vmSocks.jpeg"
-            },
-            {
-                id: 2,
-                color: "blue",
-                image: "./blueSock.jpeg"
-            }
+                variantId: 2,
+                variantColor: "blue",
+                variantImage: "./assets/blueSock.jpeg",
+                variantQuantity: 10
 
+            }
         ],
         cart: 10,
     },
@@ -47,6 +37,20 @@ var app = new Vue({ //ne
         },
         subtractFromCart() {
             this.cart -= 1
+        },
+        updatedProduct(index) {
+            console.log(index)
+            return this.selectedVariant = index
+
         }
+    },
+    computed: { 
+        title() {
+            return this.brand + " " + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        }
+        
     }
 })
